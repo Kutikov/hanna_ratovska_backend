79d00ed861c57b0924d8b0f606ff99d4c792b022
Kitsune1904 <annagumiho190423@gmail.com>
2024-09-07 11:10:17 +0300

    add helpers

---
+++ helpers/helpers.js
@@ -0,0 +1,110 @@
+const fs = require("fs");
+const {request} = require("express");
+const getRandomId = () => Math.floor(Math.random() * 100);
+
+const getData = () => {
+    const dataJson = fs.readFileSync('./users.json', 'utf-8');
+    const usersArr = JSON.parse(dataJson);
+    return usersArr
+}
+
+const getAllUsers = (req, res) => {
+    const usersArr = getData();
+    res.json(usersArr);
+}
+
+const getUser = (req, res) => {
+    const id = Number(req.params.id);
+    const usersArr = getData();
+
+    if (id) {
+        const user = usersArr.find(user => user.id === id);
+        console.log(user)
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } else {
+        res.status(400).send('No id in query');
+    }
+}
+
+const addUser = (req, res) => {
+    const {name, age, city} = req.body;
+    let userId = getRandomId();
+    const usersArr = getData();
+
+    while (usersArr.some(user => user.id === userId)) {
+        userId = getRandomId()
+    }
+
+    if(name && age && city) {
+        const user = usersArr.find(user => user.name === name);
+        if(user) {
+            res.status(409).send('User exist')
+        } else {
+            usersArr.push({
+                id: userId,
+                name: name,
+                age: age,
+                city: city
+            })
+            fs.writeFileSync('./users.json', JSON.stringify(usersArr), 'utf-8');
+            res.json(usersArr);
+        }
+    } else {
+        res.status(400).send('Not full info')
+    }
+}
+
+const setUser = (req, res) => {
+    const id = Number(req.params.id);
+    const age = req.body.age;
+    const usersArr = getData();
+
+    if(id && age) {
+        const doesIdExist = usersArr.some((user) => user.id === id);
+        if(doesIdExist) {
+            const updatedArr = usersArr.map((user) =>
+                user.id === id ?
+                    {
+                        ...user,
+                        age: age
+                    } :
+                    user);
+            res.json(updatedArr);
+            fs.writeFileSync('./users.json', JSON.stringify(updatedArr), 'utf-8');
+        } else {
+            res.status(404).send('User not found')
+        }
+    } else {
+        res.status(400).send('Not full info')
+    }
+}
+
+const deleteUser = (req, res) => {
+    const id = Number(req.params.id);
+    const usersArr = getData();
+
+    if (id) {
+        const userOnDelete = usersArr.find(user => user.id === id);
+        if(userOnDelete) {
+            const newUsersArr = usersArr.filter(user => user !== userOnDelete)
+            fs.writeFileSync('./users.json', JSON.stringify(newUsersArr));
+            res.json(newUsersArr)
+        } else {
+            res.status(404).send('User not found');
+        }
+    } else {
+        res.status(400).send('No name in query');
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    getUser,
+    addUser,
+    setUser,
+    deleteUser
+}
--- index.js
+++ index.js
@@ -2,93 +2,28 @@ const express = require('express');
 const fs = require("fs");
 const PORT = 5000;
 const app= express();
+const {getUser, getAllUsers, addUser, setUser, deleteUser} = require("./helpers/helpers");
 
 app.use(express.json());
 
-const dataJson = fs.readFileSync('./users.json', 'utf-8');
-const usersArr = JSON.parse(dataJson);
-
 app.get('/', (req, res, next) => {
-    res.json(usersArr);
+    getAllUsers(req, res)
 });
 
 app.get('/users/:id', (req, res) => {
-    const id = +req.params.id;
-
-    if (id) {
-        const user = usersArr.find(user => user.id === id);
-        console.log(user)
-        if (user) {
-            res.json(user);
-        } else {
-            res.status(404).send('User not found');
-        }
-    } else {
-        res.status(400).send('No id in query');
-    }
+    getUser(req, res)
 })
 
 app.put('/users', (req, res) => {
-    const {name, age, city} = req.body;
-
-    if(name && age && city) {
-        const user = usersArr.find(user => user.name === name);
-        if(user) {
-            res.status(409).send('User exist')
-        } else {
-            usersArr.push({
-                id: usersArr[usersArr.length - 1].id + 1,
-                name: name,
-                age: age,
-                city: city
-            })
-            fs.writeFileSync('./users.json', JSON.stringify(usersArr), 'utf-8');
-            res.json(usersArr);
-        }
-    } else {
-        res.status(400).send('Not full info')
-    }
+    addUser(req, res)
 })
 
 app.patch('/users/settings/:id', (req, res) => {
-    const id = +req.params.id;
-    const age = req.body.age;
-
-    if(id && age) {
-        const doesIdExist = usersArr.some((user) => user.id === id);
-        if(doesIdExist) {
-            const updatedArr = usersArr.map((user) =>
-                user.id === id ?
-                    {
-                        ...user,
-                        age: age
-                    } :
-                    user);
-            res.json(updatedArr);
-            fs.writeFileSync('./users.json', JSON.stringify(updatedArr), 'utf-8');
-        } else {
-            res.status(404).send('User not found')
-        }
-    } else {
-        res.status(400).send('Not full info')
-    }
+    setUser(req, res)
 })
 
 app.delete('/users/:id', (req, res) => {
-    const id = +req.params.id;
-
-    if (id) {
-        const userOnDelete = usersArr.find(user => user.id === id);
-        if(userOnDelete) {
-            const newUsersArr = usersArr.filter(user => user !== userOnDelete)
-            fs.writeFileSync('./users.json', JSON.stringify(newUsersArr));
-            res.json(newUsersArr)
-        } else {
-            res.status(404).send('User not found');
-        }
-    } else {
-        res.status(400).send('No name in query');
-    }
+    deleteUser(req, res)
 })
 
 app.listen(PORT, ()=> {
